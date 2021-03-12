0f7e9614b6c64a53a3f3e1f336173fde280f97ac
Lenodez <66466210+Lenodez@users.noreply.github.com>
2021-03-12 13:20:18 +0300

    Изменил название

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -90,9 +90,9 @@ namespace StepperControlEthernet
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(12, 64);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.Size = new System.Drawing.Size(77, 13);
             this.label2.TabIndex = 5;
-            this.label2.Text = "Адрес отправки";
+            this.label2.Text = "IP получателя";
             // 
             // commandTextBox
             // 
